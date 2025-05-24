1091118f536f33830ea5aa8461aad811c93ee9fe
seraa228321 <miaubibek@gmail.com>
2025-05-24 19:15:21 +0300

    ...

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("openPromoModal").addEventListener("click", () => {
-    document.getElementById("heroModal").style.visibility = 'visible';
-});
+document.getElementById("openPromoModal")
+    .addEventListener("click", () => {
+        document.getElementById("heroModal").style.visibility = 'visible';
+        document.body.style.overflow = 'hidden';
+    });
+
+document.getElementById("closePromoModal")
+    .addEventListener("click", () => {
+        document.getElementById("heroModal").style.visibility = 'hidden';
+        document.body.style.overflow = 'visible';
+    });
